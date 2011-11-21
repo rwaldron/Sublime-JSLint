c6026e5765e3334ccc1e958f8a076bd5b6013170
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-11-21 20:05:35 -0300

    Source Formatting

--- jslint.py
+++ jslint.py
@@ -1,59 +1,64 @@
-import sublime, sublime_plugin, subprocess, os, json
+import sublime
+import sublime_plugin
+import os
+import json
 
 package = 'sublime-jslint'
 
-def get_settings_value(key):
-	settings1 = sublime.load_settings(package + '.sublime-settings')
 
-	return settings1.get(key)
+def get_settings_value(key):
+    settings1 = sublime.load_settings(package + '.sublime-settings')
+
+    return settings1.get(key)
+
 
 class JslintCommand(sublime_plugin.TextCommand):
-	edit = {}
+    edit = {}
 
-	def notify(self, jslint):
-		images_dir = os.path.join(sublime.packages_path(), package) + '/images/'
+    def notify(self, jslint):
+        images_dir = os.path.join(sublime.packages_path(), package) + '/images/'
 
-		if jslint['totalErrors']  > 0:
-			image = 'error.png'
-		else:
-			image = 'success.png'
+        if jslint['totalErrors'] > 0:
+            image = 'error.png'
+        else:
+            image = 'success.png'
 
-		os.popen(get_settings_value("notification_command") % { "msg": jslint['file'], "title": jslint['shortlog'], "image": images_dir + image })
+        os.popen(get_settings_value("notification_command") % {"msg": jslint['file'], "title": jslint['shortlog'], "image": images_dir + image})
 
+    def run(self, edit, fn='', log_level='shortlog'):
+        syntax, extension = os.path.splitext(os.path.basename(self.view.settings().get('syntax')))
 
-	def run(self, edit, fn = '', log_level = 'shortlog'):
-		syntax, extension = os.path.splitext(os.path.basename(self.view.settings().get('syntax')))
+        if syntax == 'JavaScript':
+            self.edit = edit
 
-		if syntax == 'JavaScript':
-			self.edit = edit
+            package_dir = os.path.join(sublime.packages_path(), package)
 
-			package_dir = os.path.join(sublime.packages_path(), package)
+            if fn == '':
+                fn = self.view.file_name()
 
-			if fn == '':
-				fn = self.view.file_name()
+            print get_settings_value("node_path")
 
-			print get_settings_value("node_path")
+            jslint = json.loads(os.popen(get_settings_value("node_path") + ' "%(linter)s" --%(log_level)s "%(file)s"' % {'linter': package_dir + '/linter.js', 'log_level': log_level, 'file': fn}).read())
 
-			jslint = json.loads(os.popen(get_settings_value("node_path") + ' "%(linter)s" --%(log_level)s "%(file)s"' % { 'linter': package_dir + '/linter.js', 'log_level': log_level, 'file': fn }).read())
+            if log_level == 'all' or log_level == 'shortlog':
+                self.notify(jslint)
 
-			if log_level == 'all' or log_level == 'shortlog':
-				self.notify(jslint)
+            if log_level == 'all' or log_level == 'log':
+                self.show_panel(jslint)
 
-			if log_level == 'all' or log_level == 'log':
-				self.show_panel(jslint)
+    def show_panel(self, jslint):
+        output_view = self.view.window().get_output_panel('jslint')
 
-	def show_panel(self, jslint):
-		output_view = self.view.window().get_output_panel('jslint')
+        output_view.settings().set("result_file_regex", get_settings_value('result_file_regex'))
 
-		output_view.settings().set("result_file_regex", get_settings_value('result_file_regex'))
+        output_view.settings().set("result_line_regex", get_settings_value('result_line_regex'))
 
-		output_view.settings().set("result_line_regex", get_settings_value('result_line_regex'))
+        output_view.insert(self.edit, 0, jslint['log'])
 
-		output_view.insert(self.edit, 0, jslint['log'])
+        self.view.window().run_command('show_panel', {'panel': 'output.jslint'})
 
-		self.view.window().run_command('show_panel', { 'panel': 'output.jslint' })
 
 class ApplyJslintOnJavaScriptSave(sublime_plugin.EventListener):
-	def on_post_save(self, view):
-		if get_settings_value('show_notification_on_save'):
-			view.run_command('jslint');
+    def on_post_save(self, view):
+        if get_settings_value('show_notification_on_save'):
+            view.run_command('jslint')
